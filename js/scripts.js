ef58cc5e9b3a128e58e5a80590a74fd99f0bdf65
Joanna Bieńkowska <ferart.pl@gmail.com>
2017-03-27 14:22:26 +0200

    poprawki w kodzie

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@
 	if (!window.localStorage) return;
 
 	function LocalDB(name){
-		this._dbName = name;
+		this.dbName = name;
 	}
 
 	LocalDB.prototype.save = function(key, obj){
-		window.localStorage.setItem(this._dbName + '.' + key, JSON.stringify(obj));
+		window.localStorage.setItem(this.dbName + '.' + key, JSON.stringify(obj));
 
 	};
 
 	LocalDB.prototype.get = function(key) {
 
-		var data = window.localStorage.getItem(this._dbName + '.' + key);
+		var data = window.localStorage.getItem(this.dbName + '.' + key);
 
 		return JSON.parse(data);
 
